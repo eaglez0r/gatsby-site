699059bef15d37f2e7592d7e7c5bb55eb43a392f
eaglez0r <deidara_a_k_a@hotmail.com>
2020-06-11 00:42:05 -0300

    mixed content fix?

--- src/pages/page-6.js
+++ src/pages/page-6.js
@@ -11,13 +11,13 @@ const SixthPage = () => {
       <SEO title="Weather" />
       <div>
         {useGetWeather(
-          "http://api.openweathermap.org/data/2.5/weather?id=3448433&APPID=92a0f139f156286198cf8bac00a51dc6"
+          "https://api.openweathermap.org/data/2.5/weather?id=3448433&APPID=92a0f139f156286198cf8bac00a51dc6"
         )}
         {useGetWeather(
-          "http://api.openweathermap.org/data/2.5/weather?id=4634946&APPID=92a0f139f156286198cf8bac00a51dc6"
+          "https://api.openweathermap.org/data/2.5/weather?id=4634946&APPID=92a0f139f156286198cf8bac00a51dc6"
         )}
         {useGetWeather(
-          "http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=92a0f139f156286198cf8bac00a51dc6"
+          "https://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=92a0f139f156286198cf8bac00a51dc6"
         )}
       </div>
     </Layout>
